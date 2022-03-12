74c3a61f565ce926f07af01af5a4386bee629629
SIDI <a.sidielmoctar@gmail.com>
2022-03-12 11:44:42 +0000

    exo 3

--- 1.src/src/app/commun/components/components.module.ts
+++ 1.src/src/app/commun/components/components.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MsgSuccessComponent } from './msg-success/msg-success.component';
 import { MsgDangerComponent } from './smg-danger/msg-danger.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 
 
 @NgModule({
   declarations: [
     MsgSuccessComponent,
-    MsgDangerComponent
+    MsgDangerComponent,
+    NavBarComponent,
+    SideBarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     MsgSuccessComponent,
-    MsgDangerComponent
+    MsgDangerComponent,
+    NavBarComponent,
+    SideBarComponent
   ]
 })
 export class ComponentsModule { }
